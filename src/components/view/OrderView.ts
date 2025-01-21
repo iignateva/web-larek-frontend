4970d9dee46d2736fd9a64017aab484ecdbe5930
Irina Ignateva <ignatyeva_irina@inbox.ru>
2025-01-22 01:48:04 +0300

    fix

--- src/components/view/OrderView.ts
+++ src/components/view/OrderView.ts
@@ -16,6 +16,7 @@ export class OrderView extends Component<View> {
 	constructor(container: HTMLElement, events: IEvents) {
 		super(container);
 		this._events = events;
+		this._selectedPaymentType = null;
 
 		this._address = ensureElement<HTMLInputElement>(
 			settings.order.address,
@@ -29,23 +30,24 @@ export class OrderView extends Component<View> {
 		this._submitButton = ensureElement(settings.order.buttonToOrder, container);
 		this._formError = ensureElement(settings.order.formErrors, container);
 
+		this.setDisabled(
+			this._submitButton,
+			!(this._selectedPaymentType !== null && this._address.value !== null)
+		);
+
 		this._payOnline.addEventListener('click', () => {
 			this.setPaymentType(PaymentType.ONLINE);
+			this.checkButtonEnable();
 		});
 
 		this._payOnReceiving.addEventListener('click', () => {
 			this.setPaymentType(PaymentType.ON_RECEIVING);
+			this.checkButtonEnable();
 		});
 
 		this._address.addEventListener('input', (evt) => {
 			evt.preventDefault();
-			const isValid = this.isValid(this._formError, this._address);
-
-			if (isValid) {
-				this.setDisabled(this._submitButton, false);
-			} else {
-				this.setDisabled(this._submitButton, true);
-			}
+			this.checkButtonEnable();
 		});
 
 		this.container.addEventListener('submit', () => {
@@ -57,9 +59,20 @@ export class OrderView extends Component<View> {
 		});
 	}
 
+	private checkButtonEnable() {
+		const isValid = this.isValid(this._formError, this._address);
+
+		if (isValid && this._selectedPaymentType !== null) {
+			this.setDisabled(this._submitButton, false);
+		} else {
+			this.setDisabled(this._submitButton, true);
+		}
+	}
+
+
 	clear() {
 		this._address.value = '';
-		this.setPaymentType(null); 
+		this.setPaymentType(null);
 	}
 
 	private setPaymentType(paymentType: PaymentType | null) {
